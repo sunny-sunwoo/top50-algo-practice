808273d1dcc25a7854c4170b8d5b765d668478a6
sunny-sunwoo <sunny.sunwoo.park@gmail.com>
2019-07-13 23:43:16 -0700

    Q38 LL Cycle

---
+++ Q38_LLCycles.java
@@ -0,0 +1,42 @@
+package top50_questions;
+
+public class Q38_LLCycles {
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private static class ListNode {
+        ListNode next;
+    }
+    
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode();
+        ListNode n2 = new ListNode();
+        ListNode n3 = new ListNode();
+        ListNode n4 = new ListNode();
+        ListNode n5 = new ListNode();
+        ListNode n6 = new ListNode();
+        
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+        n6.next = n3;
+        
+        System.out.println(hasCycle(n1));
+        
+    }
+}
