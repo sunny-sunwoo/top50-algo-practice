952111c0b666bba7f84689b763396dbaed19bbf7
sunny-sunwoo <sunny.sunwoo.park@gmail.com>
2019-07-17 12:49:40 -0700

    add get unique prefix

--- Q45_Autocomplete.java
+++ Q45_Autocomplete.java
@@ -11,15 +11,17 @@ import java.util.Map;
  * 
  * [Approach] Prefix trie
  * 1. how to represent Node object?
- *    - 3 fields: 1) String prefix, 2)Map<Character, Node> children, 3) isWord - to mark the last character.
+ *    - 4 fields: 1) String prefix, 2)Map<Character, Node> children, 
+ *                3) isWord - to mark the last character, 4)counter - to find uniquePrefix.
  *    - constructor: pass-in prefix. initialize hashmap.
  *    
  * 2. Autocomplete object: 
  *    - 1 field: trie (to represent the root.)
  *    - constructor: pass in dictionary.
- *    - 2 methods: insert, findWordsByPrefix(+ helper method to find recursively)
+ *    - 3 methods: insert, findWordsByPrefix(+ helper method to find recursively), 
+ *                 getUniquePrefix(+ helper method for recursion)
  *    
- * 3. how to write the 2 methods of Autocomplete object.
+ * 3. how to write the 3 methods of Autocomplete object.
  *   1) insert - linear scan of each char in the string.
  *      1. check if trie's children map contains curr char in the keyset.
  *         - if not, put a new Node with curr char in the children map.
@@ -35,18 +37,35 @@ import java.util.Map;
  *           
  *      2. if the node of input found, find(node, result)
  *      
- *   3) helper method: find recursively
+ *   2-1) helper method: find recursively
  *      1. base case: if node.isWord is true -> add curr prefix to the result.
  *      2. traverse down to the children nodes.
+ *      
+ *   3) getUniquePrefix
+ *      1. create an AL for result.
+ *      2. call private method from the root node. with result.
+ *      3. return result
+ *      
+ *   3-1) helper method: to collect all unique prefix.
+ *      1. base case: if the children is empty
+ *      2. traverse down to all node. -> iterate over all entryset.
+ *          (1) check if it's last word && counter > 1 (e.g. dog, dogt)
+ *              -> add the prefix. (dog)
+ *          (2) check if counter == 1 (dog, dot, zebra)
+ *              -> add the prefix. (z)
+ *              -> continue. (bc/ no need to traverse more with zebra)
+ *          (3) recursive call on the child node.
+ *      
  *     
  * @author Sunny Park
  *
  */
 public class Q45_Autocomplete {
     private static class Node {
-        String prefix;
-        Map<Character, Node> children;
-        boolean isWord;
+        private final String prefix;
+        private final Map<Character, Node> children;
+        private boolean isWord;
+        private int counter = 0;
         
         Node(String prefix) {
             this.prefix = prefix;
@@ -70,10 +89,13 @@ public class Q45_Autocomplete {
                 curr.children.put(c, new Node(word.substring(0, i + 1)));
             }
             curr = curr.children.get(c);
+            curr.counter++;
             if (i == word.length() - 1) {
                 curr.isWord = true;
             }
+            System.out.println("curr: " + curr.prefix + ", " + curr.counter);
         }
+        
     }
     
     public List<String> findWordByPrefix(String input) {
@@ -101,9 +123,32 @@ public class Q45_Autocomplete {
         }
     }
     
+    public List<String> getUniquePrefix() {
+        List<String> result = new ArrayList<>();
+        getUniquePrefix(trie, result);
+        return result;
+    }
+    
+    private void getUniquePrefix(Node node, List<String> result) {
+        if (node.children.isEmpty()) return;
+        for (Map.Entry<Character, Node> entry : node.children.entrySet()) {
+            Node child = entry.getValue();
+            if (child.isWord && child.counter > 1) {
+                result.add(child.prefix);
+            }
+            if (child.counter == 1) {
+                result.add(child.prefix);
+                continue;
+            }
+            getUniquePrefix(child, result);
+        }
+    }
+    
     public static void main(String[] args) {
-        Q45_Autocomplete tester = new Q45_Autocomplete(Arrays.asList("a", "abc", "abd", "dog", "dot", "ddd"));
+        Q45_Autocomplete tester = new Q45_Autocomplete(Arrays.asList("dog", "dot", "ddd", "duck", "zebra"));
+//        Q45_Autocomplete tester = new Q45_Autocomplete(Arrays.asList("a", "abc", "abd", "dog", "dot", "ddd"));
         System.out.println(tester.findWordByPrefix("do"));
+        System.out.println(tester.getUniquePrefix());
     }
     
     
