93f38d524f90d96c0d76ad671f8e519d73aedfee
sunny-sunwoo <sunny.sunwoo.park@gmail.com>
2019-08-16 23:17:05 -0700

    note for fibo with bottom-up and top-down approach

---
+++ Q49_FibonacciNumber.java
@@ -0,0 +1,22 @@
+package top50_questions;
+/**
+ * Question: Given an integer n, write a function to compute the nth Fibonacci number.
+ * 
+ * e.g.
+ * fibonacci(1) = 1
+ * fibonacci(5) = 5
+ * fibonacci(10) = 55
+ * 
+ * [Approach1] Bottom-up
+ * sol1) using memoization array
+ * sol2) using variables
+ * 
+ * [Approach2] Top-down
+ * using recursion with hashmap
+ * 
+ * @author Sunny Park
+ *
+ */
+public class Q49_FibonacciNumber {
+
+}
